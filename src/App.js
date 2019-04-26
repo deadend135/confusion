4b0a4755105efec031e992e2c49aeeedcaa94a44
deadend135 <sharma.vikrant135@gmail.com>
2019-04-26 12:45:49 +0530

    Single Page Application Part 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
-import { Navbar, NavbarBrand } from "reactstrap";
 import { BrowserRouter } from "react-router-dom";
-import Menu from "./components/MainComponent";
-import { DISHES } from "./shared/dishes";
 import "./App.css";
 import Main from "./components/MainComponent";
 
