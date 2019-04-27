847b66effb1a1694ed2ca76676cde21d87aea00a
deadend135 <sharma.vikrant135@gmail.com>
2019-04-27 17:22:04 +0530

    Assignment 2 part 1 and part 2 completed

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -9,9 +9,30 @@ import {
 } from "reactstrap";
 import { Link } from "react-router-dom";
 
+function RenderLeader({ leader }) {
+  return (
+    <div key={leader.id} className="col-12 mt-5">
+      <Media tag="li">
+        <Media left middle>
+          <Media object src={leader.image} alt={leader.name} />
+        </Media>
+        <Media body className="ml-5">
+          <Media heading>{leader.name}</Media>
+          <p>{leader.designation}</p>
+          <p>{leader.description}</p>
+        </Media>
+      </Media>
+    </div>
+  );
+}
+
 function About(props) {
   const leaders = props.leaders.map(leader => {
-    return <p>Leader {leader.name}</p>;
+    return (
+      <div>
+        <RenderLeader leader={leader} />
+      </div>
+    );
   });
 
   return (
